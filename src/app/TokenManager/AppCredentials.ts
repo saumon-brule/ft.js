626cb7d35f48c7b94ee98ce89eb20b94816620bc
saumon-brule <96207807+saumon-brule@users.noreply.github.com>
2025-08-02 07:17:43 +0200

    refactor: improve Credential data security and behavior of secure getters

--- src/app/TokenManager/AppCredentials.ts
+++ src/app/TokenManager/AppCredentials.ts
@@ -13,19 +13,22 @@ export class AppCredentials {
 		this.requestNewToken();
 	}
 
-	private _getSecureTokenData<T extends keyof AppTokenData>(dataName: T): AppTokenData[T] {
+	private get _data(): AppTokenData {
 		if (this._tokenData === undefined) throw new Error("Uninitalized token: Cannot access data of uninitialized token");
 		if (this._tokenData === null) throw new Error("Invalid token: Token is invalid, try to get a new one");
-		return this._tokenData[dataName];
+		return this._tokenData;
 	}
 
-	get token(): string { return this._getSecureTokenData("access_token"); }
-	get type(): string { return this._getSecureTokenData("token_type"); }
-	get expiresIn(): number { return this._getSecureTokenData("expires_in"); }
-	get createdAt(): number { return this._getSecureTokenData("created_at"); }
-	get expiresAt(): number { return this._getSecureTokenData("created_at") + this._getSecureTokenData("expires_in"); }
-	get scope(): string { return this._getSecureTokenData("scope"); }
-	get secretValidUntil(): number { return this._getSecureTokenData("created_at"); }
+	get token(): string { return this._data.access_token; }
+	get type(): string { return this._data.token_type; }
+	get expiresIn(): number { return this._data.expires_in; }
+	get createdAt(): number { return this._data.created_at; }
+	get scope(): string { return this._data.scope; }
+	get secretValidUntil(): number { return this._data.created_at; }
+	get expiresAt(): number {
+		const data = this._data;
+		return data.created_at + data.expires_in;
+	}
 
 	async requestNewToken() {
 		if (this._refreshPromise) return this._refreshPromise;
--- src/user/UserCredential.ts
+++ src/user/UserCredential.ts
@@ -13,19 +13,22 @@ export class UserCredential {
 		this.oauthConfig = oauthConfig;
 	}
 
-	private _getSecureTokenData<T extends keyof UserTokenData>(dataName: T): UserTokenData[T] {
+	private get _data(): UserTokenData {
 		if (!this._tokenData) throw new Error("Invalid token: Token is invalid, try to get a new one");
-		return this._tokenData[dataName];
+		return this._tokenData;
 	}
 
-	get token(): string { return this._getSecureTokenData("access_token"); }
-	get refreshToken(): string { return this._getSecureTokenData("refresh_token"); }
-	get type(): string { return this._getSecureTokenData("token_type"); }
-	get expiresIn(): number { return this._getSecureTokenData("expires_in"); }
-	get createdAt(): number { return this._getSecureTokenData("created_at"); }
-	get expiresAt(): number { return this._getSecureTokenData("created_at") + this._getSecureTokenData("expires_in"); }
-	get scope(): string { return this._getSecureTokenData("scope"); }
-	get secretValidUntil(): number { return this._getSecureTokenData("secret_valid_until"); }
+	get token(): string { return this._data.access_token; }
+	get refreshToken(): string { return this._data.refresh_token; }
+	get type(): string { return this._data.token_type; }
+	get expiresIn(): number { return this._data.expires_in; }
+	get createdAt(): number { return this._data.created_at; }
+	get scope(): string { return this._data.scope; }
+	get secretValidUntil(): number { return this._data.secret_valid_until; }
+	get expiresAt(): number {
+		const data = this._data;
+		return data.created_at + data.expires_in;
+	}
 
 	async requestNewToken() {
 		if (this._refreshPromise) return this._refreshPromise;
