46894cf15b5710654129d8d79f5f89aa288a3870
saumon-brule <96207807+saumon-brule@users.noreply.github.com>
2025-08-27 20:55:06 +0200

    feat: expose fetch function in import in "requests" object

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import { fetchMe } from "./api/me.js";
+import { fetchAppToken, fetchRefreshUserToken, fetchUserToken } from "./api/oauth/token.js";
 import { FtApp } from "./app/App.js";
 export default FtApp;
 
 export { AuthenticatedRequest } from "~/structures/AuthenticatedRequest.js";
 export { default as FtRequestParams } from "./generic/ftRequestParams/FtRequestParams.js";
+
+export const requests = {
+	me: fetchMe,
+	oauth: {
+		appToken: fetchAppToken,
+		userToken: fetchUserToken,
+		refreshUserToken: fetchRefreshUserToken
+	}
+};
